9fed400cbc3fffcda7ed44d73ca2ff4cb96b70fc
drox13 <dario.baron@uptc.edu.co>
2021-09-23 00:26:36 -0500

    se optiene la hora de la pai y se instalo socket.io

--- controller/monitor.js
+++ controller/monitor.js
@@ -3,23 +3,11 @@ const axios = require('axios');
 setInterval(() => {
     axios.get("http://worldtimeapi.org/api/timezone/America/Bogota")
     .then(({data}) => {
-        var time = new Date(data.datetime);
-        console.log(data.datetime);
+        let time = new Date(data.utc_datetime);
+        console.log("hora de Bogota");
         console.log(time.getHours(), time.getMinutes(), time.getSeconds());
-        console.log(time , "tiempo");
     })
     .catch(error=>{
         console.log(error);
-      });
+    });
 }, 1000); //60000 = 1 minuto
-
-
-var timeApi = axios.get("http://worldtimeapi.org/api/timezone/America/Bogota")
-    .then(({data}) => {
-        var time = new Date(data.datetime);
-        console.log(data.datetime);
-        console.log(time.getHours(), time.getMinutes(), time.getSeconds())
-    })
-    .catch(error=>{
-        console.log(error);
-      });
